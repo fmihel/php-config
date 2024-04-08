10a1c04ae5e66badd28c3939cf92648e6e2b74a8
fmihel <fmihel76@gmail.com>
2024-04-08 09:49:27 +0300

    fix loadFromFile

--- source/ConfigCore.php
+++ source/ConfigCore.php
@@ -2,56 +2,75 @@
 
 namespace fmihel\config;
 
+use fmihel\console;
 
-/** 
+/**
  * загрузка файла конфигурации
-*/
-class ConfigCore{
+ */
+class ConfigCore
+{
 
-    public $param=[];
+    public $param = [];
     private $_test = false;
 
-    private $settings=[
-        'fileName'=>'config.php',
-        'configVarName'=>'config',
-        'templateVarName'=>'configTemplate',
-        'appPath'=>'',
-        'template'=>'',
+    private $settings = [
+        'fileName' => 'config.php',
+        'configVarName' => 'config',
+        'templateVarName' => 'configTemplate',
+        'appPath' => '',
+        'template' => '',
     ];
-    
-    function __construct(Array $settings=[]){
-        $appPath = dirname($_SERVER['SCRIPT_FILENAME']).'/';
-        $template = $appPath.'config.template.php';
+
+    function __construct(array $settings = [])
+    {
+        $appPath = dirname($_SERVER['SCRIPT_FILENAME']) . '/';
+        $template = $appPath . 'config.template.php';
         $this->settings = array_merge(
             $this->settings,
             [
-                'appPath'=>$appPath,
-                'template'=>$template,
+                'appPath' => $appPath,
+                'template' => $template,
             ],
             $settings
         );
 
-        if (file_exists($this->settings['fileName']))
+        if (file_exists($this->settings['fileName'])) {
             $this->loadFromFile($this->settings['fileName']);
-        if (file_exists($this->settings['template']))
+        }
+
+        if (file_exists($this->settings['template'])) {
             $this->test();
+        }
+
     }
-    /** 
-     * загрузка конфига из файла (добавит или перезапишет текущую конфигурацию) 
-     * 
-    */
-    public function loadFromFile(string $file){
-        $configVarName = $this->settings['configVarName'];
-        require_once $file;
-        $this->param = array_merge($this->param,${$configVarName});
+    /**
+     * загрузка конфига из файла (добавит или перезапишет текущую конфигурацию)
+     *
+     */
+    public function loadFromFile(string $file)
+    {
+        try {
+            $configVarName = $this->settings['configVarName'];
+            if (!file_exists($file)) {
+                throw new \Exception('config ' . $file . ' not exists');
+            }
+
+            require $file;
+            $this->param = array_merge($this->param, ${$configVarName});
+
+        } catch (\Exception $e) {
+            console::error($e);
+        };
     }
     /** сравнение шаблона и загруженного конфига */
-    public function test(string $templateFileName=''){
+    public function test(string $templateFileName = '')
+    {
 
-        if ($templateFileName==='')
+        if ($templateFileName === '') {
             $templateFileName = $this->settings['template'];
-        
-        if (file_exists($templateFileName)){
+        }
+
+        if (file_exists($templateFileName)) {
 
             $templateVarName = $this->settings['templateVarName'];
             require_once $templateFileName;
@@ -59,73 +78,86 @@ class ConfigCore{
             //error_log(print_r($template,true));
             $keys = array_keys($template);
             $errors = [];
-            $warns  = [];
-            foreach($keys as $key){
-                if (!isset($this->param[$key])){
-                    $errors[] = "config var [$key] not exists, need define [$key] as ".$template[$key];
+            $warns = [];
+            foreach ($keys as $key) {
+                if (!isset($this->param[$key])) {
+                    $errors[] = "config var [$key] not exists, need define [$key] as " . $template[$key];
                 }
             };
             $keys = array_keys($this->param);
-            foreach($keys as $key){
-                if (!isset($template[$key])){
+            foreach ($keys as $key) {
+                if (!isset($template[$key])) {
                     $warns[] = "[$key] is define in config,but not defined in template";
                 }
             };
 
-            if (count($errors)>0){
-                $this->stop(['errors'=>$errors,'warns'=>$warns]);
+            if (count($errors) > 0) {
+                $this->stop(['errors' => $errors, 'warns' => $warns]);
             };
 
-            if (count($warns)>0){
-                foreach($warns as $warn)
-                error_log('Warn: '.$warn);
+            if (count($warns) > 0) {
+                foreach ($warns as $warn) {
+                    error_log('Warn: ' . $warn);
+                }
+
             }
-        }else{
-            $this->stop(['msg'=>'not exists template file: '.$templateFileName]);
+        } else {
+            $this->stop(['msg' => 'not exists template file: ' . $templateFileName]);
         }
     }
-    
-    public function define($name,$mean){
-        if (!isset($this->param[$name])){
+
+    public function define($name, $mean)
+    {
+        if (!isset($this->param[$name])) {
             $this->param[$name] = $mean;
         }
     }
 
-    /** 
+    /**
      * name || name ,default
-    */
-    public function get(...$param){
-        
-        $count = count($param);
-        if ($count === 0)
-            throw new \Exception("need set one or two params", 0);
-            
-        $name = $param[0];        
+     */
+    public function get(...$param)
+    {
 
-        if ( !isset($this->param[$name]) ){
-            if ($count === 1)    
+        $count = count($param);
+        if ($count === 0) {
+            throw new \Exception("need set one or two params", 0);
+        }
+
+        $name = $param[0];
+
+        if (!isset($this->param[$name])) {
+            if ($count === 1) {
                 throw new \Exception("param [$name] is not exists", 0);
+            }
+
             return $param[1];
         };
 
         return $this->param[$name];
     }
-    private function stop($params=[]){
-        $params=array_merge([
-            'caption'=>'Config test stop webapp:',
-            'errors'=>[],
-            'warns'=>[],
-            'msg'=>'',
-        ],$params);
+    private function stop($params = [])
+    {
+        $params = array_merge([
+            'caption' => 'Config test stop webapp:',
+            'errors' => [],
+            'warns' => [],
+            'msg' => '',
+        ], $params);
         echo '<body style="background:white;color:black;font-family:Courier;font-size:12px">';
-        echo '<div style="background:gray;color:white;padding:2px">'.$params['caption'].'</div>';
-        if ($params['msg']!=='')
-            echo '<span style="color:red">'.$params['msg']."</span><br>";
+        echo '<div style="background:gray;color:white;padding:2px">' . $params['caption'] . '</div>';
+        if ($params['msg'] !== '') {
+            echo '<span style="color:red">' . $params['msg'] . "</span><br>";
+        }
 
-        foreach($params['errors'] as $error)
-            echo '<span style="color:red">Error: '.$error."</span><br>";
-        foreach($params['warns'] as $warn)
-            echo '<span style="color:#F09A4D">Warn : '.$warn."</span><br>";
+        foreach ($params['errors'] as $error) {
+            echo '<span style="color:red">Error: ' . $error . "</span><br>";
+        }
+
+        foreach ($params['warns'] as $warn) {
+            echo '<span style="color:#F09A4D">Warn : ' . $warn . "</span><br>";
+        }
+
         echo '</body>';
         exit(0);
     }
